f326fe3186ddd7e7ef82528086205ba13cd1ef9a
Ruslan <rus.beluaev.101@gmail.com>
2025-02-10 18:27:36 +0700

    В стадии разработки

---
+++ 1.py
@@ -0,0 +1,42 @@
+from src.generators import filter_by_currency, transaction_descriptions, card_number_generator, transactions, transactions_clear
+
+
+#generator = filter_by_currency(transactions, "RUB")
+
+#print(next(generator))
+#print(next(generator))
+
+
+
+#def transaction_descriptions(transactions: list):
+    #"""Выводит какая операция была произведена"""
+    #limit = 0
+    #for i in transactions:
+    #    limit += 1
+     #   yield i["description"]
+    #if limit == len(transactions):
+    #    yield "Список закончился"
+    #    return
+
+
+#descriptions = transaction_descriptions(transactions_clear)
+
+#print("Перевод организации\nПеревод со счета на счет\nПеревод со счета на счет\nПеревод с карты на карту\nПеревод организации")
+
+#a = ["Перевод организации","Перевод со счета на счет","Перевод со счета на счет","Перевод с карты на карту","Перевод организации", "Список закончился"]
+#for i in a:
+    #print(i)
+
+a = 11
+b = 12
+if a < 11 or b > 10:
+    print("dd")
+
+
+
+
+
+
+
+
+
